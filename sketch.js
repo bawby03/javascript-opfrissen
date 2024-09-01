54b19b664ebbdebd5f9df3262fa88129e3189c9f
bawby03 <113313@emmauscollege.nl>
2024-09-01 20:43:43 +0000

    MAND

--- sketch.js
+++ sketch.js
@@ -16,12 +16,13 @@ let appelX = 600; // x-positie van appel
 let appelY = 55;  // y-positie van appel
 let appelBreedte = 50;
 let mandY = 650;
-let mandBreedte = 100;
+let mandBreedte = 150;
 let mandHoogte = 15;
 let appelV = 10;
 let score;
 let punt;
 let img1;
+let img2;
 
 
 /* ********************************************* */
@@ -39,6 +40,7 @@ function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
   createCanvas(1280, 720);
   img1 = loadImage('appel.png');
+  img2 = loadImage('mand.webp');
 }
 
 /**
@@ -56,9 +58,15 @@ function draw() {
   ellipse(appelX, appelY, appelBreedte, appelBreedte);
   image(img1,appelX - 50, appelY - 50, appelBreedte * 2, appelBreedte * 2);
   appelY = appelY + appelV;
+  // if (score++){
+  //   appelV= appelV + 0.1;
+  // }
+
   if(mouseX >= 0 && mouseX + mandBreedte <= 1280){
     rect(mouseX, mandY, mandBreedte, mandHoogte);
+    image(img2, mouseX - 20, mandY - 85, mandBreedte + 40, mandHoogte + 100);
   }
+
   if (appelY > 720){
     randomizer();
   }
@@ -68,9 +76,11 @@ function draw() {
     punt = true;
       if(punt === true){
       score++;
+      appelV = appelV + 0.1;
       punt = false;
       }
   }
+
   textSize(50);
   fill(255,255,255);
   text("score: " + score, 50, 50);
